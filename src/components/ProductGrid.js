cdfe8bceabcd969c6fb935e1c4575ad59adcb1fb
Your Name <abdullahbaloch070@gmail.com>
2025-01-08 19:02:43 +0500

    modified:   src/components/ProductGrid.js

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -3,9 +3,9 @@ import ProductCard from "./ProductCard";
 
 const ProductGrid = ({ products, onAddToCart }) => {
   return (
-    <div className="flex flex-wrap gap-4 justify-start">
+    <div className="flex flex-wrap gap-4 justify-start" style={{ position:'relative', left:'10%',top:'35%'}} >
       {products && products.length > 0 && products.map((product) => (
-        <div className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4" key={product.id}> {/* Responsive width for each product card */}
+        <div style={{width:'20%'}} className="col-3" key={product.id}> {/* Responsive width for each product card */}
           <ProductCard product={product} onAddToCart={onAddToCart} />
         </div>
       ))}
