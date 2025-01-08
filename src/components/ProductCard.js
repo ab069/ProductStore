5dc2ddbc9dd6c1a37848fbcda3b90117e316621b
Your Name <abdullahbaloch070@gmail.com>
2025-01-08 15:57:52 +0500

    Resolved merge conflictqs

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,7 +2,7 @@
 const ProductCard = ({ product, onAddToCart }) => {
   return (
     <div className="border p-4 rounded-lg">
-      <img src={product.image} alt={product.name} style={{height:'100px'}} className="w-32 h-32 object-cover mb-4" />
+      <img src={product.image} alt={product.name} style={{height:'100 px'}} className="w-32 h-32 object-cover mb-4" />
       <h3 className="font-semibold">{product.name}</h3>
       <p className="text-sm text-gray-600">{product.description}</p>
       <p className="font-bold">${product.price}</p>
